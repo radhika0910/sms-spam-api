d674d2bddc47048cce81315b23f614752283672c
Radhika Bhoyar <97309084+radhika0910@users.noreply.github.com>
2023-04-24 23:28:53 +0530

    app addition

---
+++ app.py
@@ -0,0 +1,37 @@
+from flask import Flask,request,jsonify
+import numpy as np
+import new
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+from new import transform_text
+
+# model1 = pickle.load(open('model1.pkl','rb'))
+tfidf = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('model.pkl','rb'))
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "Hello world"
+
+
+@app.route('/sms-spam',methods=['POST'])
+def smsspam():
+    input_sms=request.form.get('input_sms')
+    transformed_sms = new.transform_text(input_sms)
+    vector_input = tfidf.transform([transformed_sms])
+    result = model.predict(vector_input)[0]
+    if result == 1:
+        answer= "spam"
+    else:
+        answer= "not spam"
+    
+    return jsonify({'answer':str(answer)})
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0",port=5000)
---
+++ new.py
@@ -0,0 +1,31 @@
+import pickle
+import string
+from nltk.corpus import stopwords
+import nltk
+from nltk.stem.porter import PorterStemmer
+
+
+def transform_text(text):
+    ps = PorterStemmer()
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+    return " ".join(y)
